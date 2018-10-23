ecf895e279df26149c462252deee5a267c9dedd5
SkyeN <skye@dames.es>
2018-10-22 17:15:57 -0700

    add the hero editor, displaying the hero list and showing master/detail components

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../models/hero-model';
+import {HEROES} from '../models/mock-heroes-model';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+  heroes = HEROES;
+  selectedHero: Hero;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
